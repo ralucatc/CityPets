e88d02ded0f53c874d9325ee1d0b0baf328f614f
DenisaPG-11 <papdeni11@gmail.com>
2021-03-15 20:39:03 +0200

    Connect firebase to expo app

--- App.js
+++ App.js
@@ -2,6 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as firebase from "firebase"
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBVwrcl0YFwbSzW8CIcW1_amI6vtzu6Rus",
+  authDomain: "pets-friendly-cities.firebaseapp.com",
+  databaseURL: "https://pets-friendly-cities-default-rtdb.firebaseio.com",
+  projectId: "pets-friendly-cities",
+  storageBucket: "pets-friendly-cities.appspot.com",
+  messagingSenderId: "871709221126",
+  appId: "1:871709221126:web:b5fcd36b1b1876b7a2be87",
+  measurementId: "G-85XBB4KVXR"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 export default function App() {
   return (
     <View style={styles.container}>
@@ -14,7 +30,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFB6C1',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
