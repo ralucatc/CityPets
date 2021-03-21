7a9471b961687aeb9477d005d144f288c7317f58
Raluca <carare.raluca27@gmail.com>
2021-03-22 00:21:40 +0200

    Added the welcome page and two buttons

---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { ImageBackground, StyleSheet, View } from 'react-native';
+import AppButton from '../components/AppButton';
+import colors from '../config/colors';
+
+function WelcomeScreen(props) {
+    return (
+       <ImageBackground 
+       style={styles.background}
+       source={require("../assets/background.png")}>
+           <View style={styles.buttonsContainer}>
+           <AppButton title="Login" />
+           <AppButton title="Register" color = "secondary" />
+           </View>
+       </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        justifyContent: "flex-end",
+    },
+
+    buttonsContainer: {
+        padding: 20,
+        width: "100%",
+    },
+    
+})
+export default WelcomeScreen;
