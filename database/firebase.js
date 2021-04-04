0afec9d8a262c7e7989da37f42aca048bf3b90be
Raluca <carare.raluca27@gmail.com>
2021-04-04 18:17:49 +0300

    firebase update

--- database/firebase.js
+++ database/firebase.js
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-import * as firebase from "firebase";
-import "firebase/auth";
-import "firebase/database";
-import "firebase/firestore";
-import "firebase/functions";
-import "firebase/storage"
-=======
 import * as firebase from 'firebase';
->>>>>>> origin/master
 
 var firebaseConfig = {
   apiKey: "AIzaSyBVwrcl0YFwbSzW8CIcW1_amI6vtzu6Rus",
@@ -20,10 +11,6 @@ var firebaseConfig = {
   measurementId: "G-85XBB4KVXR"
 };
 
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/master
 firebase.initializeApp(firebaseConfig);
  
 export default firebase;
